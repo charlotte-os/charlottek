f8034a57b8fede752b9293e62383a8daa6cdf8af
Mohit Patel <mdpatel.csecon@outlook.com>
2025-01-15 23:48:59 -0500

    Fixed the PMM self test so it doesn't deadlock

--- nova/src/self_test/memory/pmem.rs
+++ nova/src/self_test/memory/pmem.rs
@@ -5,24 +5,33 @@ use crate::memory::pmem::PHYSICAL_FRAME_ALLOCATOR;
 pub fn test_pmem() {
     logln!("Starting physical memory subsystem tests...");
     logln!("Attempting to allocate a physical memory frame.");
-    if let Ok(frame) = PHYSICAL_FRAME_ALLOCATOR.lock().allocate_frame() {
-        logln!("Allocated a frame at {:?}.", frame);
-        let magic_number = 0xCAFEBABEu32;
-        logln!("Writing magic number 0x{:X} to the beginning of the frame.", magic_number);
-        let frame_ptr = <PAddr as Into<*mut u32>>::into(frame.clone());
-        unsafe {
-            frame_ptr.write(magic_number);
-            logln!("Reading back magic number from the frame: {:X}", (frame_ptr.read()));
+
+    let mut pfa_lock = PHYSICAL_FRAME_ALLOCATOR.lock();
+
+    match pfa_lock.allocate_frame() {
+        Ok(ref frame) => {
+            logln!("Allocated a frame at {:?}.", frame);
+            let magic_number = 0xCAFEBABEu32;
+            logln!("Writing magic number 0x{:X} to the beginning of the frame.", magic_number);
+            let frame_ptr = <PAddr as Into<*mut u32>>::into(*frame);
+            unsafe {
+                frame_ptr.write(magic_number);
+                logln!("Reading back magic number from the frame: {:X}", (frame_ptr.read()));
+            }
+            match pfa_lock.deallocate_frame(*frame) {
+                Ok(()) => {
+                    logln!("Successfully deallocated frame.");
+                },
+                Err(e) => {
+                    logln!("Failed to deallocate frame!");
+                    panic!("Self-test failure: Failed to deallocate a physical memory frame at address {:?}. Error: {:?}", frame, e);
+                }
+            }
+        },
+        Err(e) => {
+            logln!("Failed to allocate a frame!");
+            panic!("Self-test failure: Failed to allocate a frame from the physical frame allocator. Error: {:?}", e);
         }
-        if let Ok(()) = PHYSICAL_FRAME_ALLOCATOR.lock().deallocate_frame(frame) {
-            logln!("Successfully deallocated frame.");
-        } else {
-            logln!("Failed to deallocate frame!");
-            panic!("Self-test failure: Failed to deallocate a physical memory frame at address {:?}", frame);
-        }
-    } else {
-        logln!("Failed to allocate a frame!");
-        panic!("Self-test failure: Failed to allocate a frame from the physical frame allocator");
     }
     logln!("All physical memory subsystem tests passed.")
 }
