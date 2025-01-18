8f2df111301a807a659dff4f3054ab75303d98aa
Mohit D Patel <mdpatel.csecon@outlook.com>
2025-01-17 22:47:30 -0500

    Laid down the basic skeleton for the virtual memory manager both outside the hal and the ISA specific interface within it as well as how they connect.

--- nova/src/hal/isa/interface/memory/mod.rs
+++ nova/src/hal/isa/interface/memory/mod.rs
@@ -1,6 +1,17 @@
 pub mod address;
 
+use crate::memory::vmem::MemoryMapping;
+
 pub trait MemoryInterface {
     type VAddr: address::VirtualAddress;
     type PAddr: address::PhysicalAddress;
+    type Error;
+
+    fn find_free_region(addr_space: usize, n_pages: usize) -> Result<Self::VAddr, Self::Error>;
+    fn map_page(addr_space: usize, mapping: MemoryMapping) -> Result<(), Self::Error>;
+    fn unmap_page(addr_space: usize, vaddr: Self::VAddr) -> Result<MemoryMapping, Self::Error>;
+    fn is_mapped(addr_space: usize, vaddr: Self::VAddr) -> Result<bool, Self::Error>;
+    fn get_mapping(addr_space: usize, vaddr: Self::VAddr) -> Result<MemoryMapping, Self::Error>;
 }
+
+
