11996ff8fc39a147389971a4a1dc101e0afa08e2
Mohit D Patel <mdpatel.csecon@outlook.com>
2025-01-17 22:47:30 -0500

    Laid down the basic skeleton for the virtual memory manager both outside the hal and the ISA specific interface within it as well as how they connect.

--- nova/src/memory/vmem/mod.rs
+++ nova/src/memory/vmem/mod.rs
@@ -1,1 +1,16 @@
+use crate::hal::isa::interface::memory::MemoryInterface;
 
+pub enum PageType {
+    Memory,
+    Mmio,
+    Framebuffer,
+}
+
+pub struct MemoryMapping {
+    pub vaddr: usize,
+    pub paddr: usize,
+    pub page_type: PageType,
+    pub read: bool,
+    pub write: bool,
+    pub execute: bool,
+}
