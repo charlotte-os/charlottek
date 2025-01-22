6c446ad9e4feaff6d009b149c713b647828ef690
Mohit Patel <mdpatel.csecon@outlook.com>
2025-01-22 01:41:39 -0500

    Started implementing the virtual memory manager

--- purrnel/src/hal/isa/x86_64/memory/mod.rs
+++ purrnel/src/hal/isa/x86_64/memory/mod.rs
@@ -7,4 +7,16 @@ pub struct MemoryInterfaceImpl;
 impl MemoryInterface for MemoryInterfaceImpl {
     type PAddr = address::paddr::PAddr;
     type VAddr = address::vaddr::VAddr;
+    type Error = Error;
+
+/*  fn find_free_region(addr_space: usize, n_pages: usize) -> Result<Self::VAddr, Self::Error>;
+    fn map_page(addr_space: usize, mapping: MemoryMapping) -> Result<(), Self::Error>;
+    fn unmap_page(addr_space: usize, vaddr: Self::VAddr) -> Result<MemoryMapping, Self::Error>;
+    fn is_mapped(addr_space: usize, vaddr: Self::VAddr) -> Result<bool, Self::Error>;
+    fn get_mapping(addr_space: usize, vaddr: Self::VAddr) -> Result<MemoryMapping, Self::Error>;
+*/
+}
+
+pub enum Error {
+
 }
