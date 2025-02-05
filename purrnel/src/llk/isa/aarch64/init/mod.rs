787743a23958ce0e8aefd70ec7c2286323f337c9
Mohit D Patel <mdpatel.csecon@outlook.com>
2025-02-05 16:22:27 -0500

    Cleanup after the messy rebase onto main

--- purrnel/src/llk/isa/aarch64/init/mod.rs
+++ purrnel/src/llk/isa/aarch64/init/mod.rs
@@ -1,5 +1,5 @@
 use super::interrupts::load_ivt;
-use crate::hal::isa::interface::init::InitInterface;
+use crate::llk::isa::interface::init::InitInterface;
 use crate::logln;
 
 pub struct IsaInitializer;
--- purrnel/src/llk/isa/x86_64/memory/mod.rs
+++ purrnel/src/llk/isa/x86_64/memory/mod.rs
@@ -1,132 +1,18 @@
 pub mod address;
 pub mod paging;
 
-use core::arch::asm;
+use crate::llk::isa::interface::memory::MemoryInterface;
 
-use crate::llk::isa::interface::memory::{AddressSpaceInterface, MemoryInterface, MemoryMapping};
 
+pub enum Error {
+    Unmapped, 
+    AlreadyMapped,
+}
 pub struct MemoryInterfaceImpl;
 
 impl MemoryInterface for MemoryInterfaceImpl {
-    type AddressSpace = AddressSpace;
+    type AddressSpace = paging::AddressSpace;
     type Error = Error;
     type PAddr = address::paddr::PAddr;
     type VAddr = address::vaddr::VAddr;
-}
-
-pub enum Error {}
-
-pub struct AddressSpace {
-    // control register 3 i.e. page table base register
-    cr3: u64,
-}
-
-impl AddressSpaceInterface for AddressSpace {
-    fn get_current() -> Self {
-        let cr3: u64;
-        unsafe {
-            asm!("mov {}, cr3", out(reg) cr3);
-        }
-        AddressSpace { cr3 }
-    }
-
-    fn load(&self) -> Result<(), <MemoryInterfaceImpl as MemoryInterface>::Error> {
-        unsafe {
-            asm!("mov cr3, {}", in(reg) self.cr3);
-        }
-        Ok(())
-    }
-
-    fn find_free_region(
-        &self,
-        n_pages: usize,
-    ) -> Result<<MemoryInterfaceImpl as MemoryInterface>::VAddr, <MemoryInterfaceImpl as MemoryInterface>::Error> {
-        todo!()
-    }
-
-    fn map_page(&mut self, mapping: MemoryMapping) -> Result<(), <MemoryInterfaceImpl as MemoryInterface>::Error> {
-        todo!()
-    }
-
-    fn unmap_page(
-        &mut self,
-        vaddr: <MemoryInterfaceImpl as MemoryInterface>::VAddr,
-    ) -> Result<MemoryMapping, <MemoryInterfaceImpl as MemoryInterface>::Error> {
-        todo!()
-    }
-
-    fn is_mapped(
-        &self,
-        vaddr: <MemoryInterfaceImpl as MemoryInterface>::VAddr,
-    ) -> Result<bool, <MemoryInterfaceImpl as MemoryInterface>::Error> {
-        todo!()
-    }
-
-    fn get_mapping(
-        &self,
-        vaddr: <MemoryInterfaceImpl as MemoryInterface>::VAddr,
-    ) -> Result<MemoryMapping, <MemoryInterfaceImpl as MemoryInterface>::Error> {
-        todo!()
-    }
-}
-
-pub enum Error {}
-
-pub struct AddressSpace {
-    // control register 3 i.e. page table base register
-    cr3: u64,
-}
-
-impl AddressSpaceInterface for AddressSpace {
-    fn get_current() -> Self {
-        let cr3: u64;
-        unsafe {
-            asm!("mov {}, cr3", out(reg) cr3);
-        }
-        AddressSpace { cr3 }
-    }
-
-    fn load(&self) -> Result<(), <MemoryInterfaceImpl as MemoryInterface>::Error> {
-        unsafe {
-            asm!("mov cr3, {}", in(reg) self.cr3);
-        }
-        Ok(())
-    }
-
-    fn find_free_region(
-        &self,
-        n_pages: usize,
-    ) -> Result<<MemoryInterfaceImpl as MemoryInterface>::VAddr, <MemoryInterfaceImpl as MemoryInterface>::Error> {
-        todo!()
-    }
-
-    fn map_page(&mut self, mapping: MemoryMapping) -> Result<(), <MemoryInterfaceImpl as MemoryInterface>::Error> {
-        todo!()
-    }
-
-    fn unmap_page(
-        &mut self,
-        vaddr: <MemoryInterfaceImpl as MemoryInterface>::VAddr,
-    ) -> Result<MemoryMapping, <MemoryInterfaceImpl as MemoryInterface>::Error> {
-        todo!()
-    }
-
-    fn is_mapped(
-        &self,
-        vaddr: <MemoryInterfaceImpl as MemoryInterface>::VAddr,
-    ) -> Result<bool, <MemoryInterfaceImpl as MemoryInterface>::Error> {
-        todo!()
-    }
-
-    fn get_mapping(
-        &self,
-        vaddr: <MemoryInterfaceImpl as MemoryInterface>::VAddr,
-    ) -> Result<MemoryMapping, <MemoryInterfaceImpl as MemoryInterface>::Error> {
-        todo!()
-    }
-}
-
-pub enum Error {
-    Unmapped,
-    AlreadyMapped,
 }
