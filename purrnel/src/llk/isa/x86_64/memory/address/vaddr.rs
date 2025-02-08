3d783af9bb2471c172ab673818b09dd76698b8e1
Mohit D Patel <mdpatel.csecon@outlook.com>
2025-02-08 15:04:36 -0500

    Change page_table_entry.rs to pte.rs, add page table index functions to VAddr for x86_64, develop the page table hierarchy walker

--- purrnel/src/llk/isa/x86_64/memory/address/vaddr.rs
+++ purrnel/src/llk/isa/x86_64/memory/address/vaddr.rs
@@ -5,6 +5,42 @@ pub struct VAddr {
     raw: usize,
 }
 
+/// VAddr component indexes and masks
+const PAGE_TABLE_INDEX_MASK: usize = 0x1ff;
+const PML4_INDEX_SHIFT: usize = 39;
+const PML4_INDEX_MASK: usize = PAGE_TABLE_INDEX_MASK << PML4_INDEX_SHIFT;
+const PDPT_INDEX_SHIFT: usize = 30;
+const PDPT_INDEX_MASK: usize = PAGE_TABLE_INDEX_MASK << PDPT_INDEX_SHIFT;
+const PD_INDEX_SHIFT: usize = 21;
+const PD_INDEX_MASK: usize = PAGE_TABLE_INDEX_MASK << PD_INDEX_SHIFT;
+const PT_INDEX_SHIFT: usize = 12;
+const PT_INDEX_MASK: usize = PAGE_TABLE_INDEX_MASK << PT_INDEX_SHIFT;
+const OFFSET_MASK: usize = 0xfff;
+
+impl VAddr {
+    /// Convenience functions to get the index for each level of the page table hierarchy
+
+    pub fn pml4_index(&self) -> usize {
+        (self.raw & PML4_INDEX_MASK) >> PML4_INDEX_SHIFT
+    }
+
+    pub fn pdpt_index(&self) -> usize {
+        (self.raw & PDPT_INDEX_MASK) >> PDPT_INDEX_SHIFT
+    }
+
+    pub fn pd_index(&self) -> usize {
+        (self.raw & PD_INDEX_MASK) >> PD_INDEX_SHIFT
+    }
+
+    pub fn pt_index(&self) -> usize {
+        (self.raw & PT_INDEX_MASK) >> PT_INDEX_SHIFT
+    }
+
+    pub fn page_offset(&self) -> usize {
+        self.raw & OFFSET_MASK
+    }
+}
+
 impl VirtualAddress for VAddr {
     fn from_ptr<T>(ptr: *const T) -> Self {
         VAddr { raw: ptr as usize }
