16dafe877562cc52fa9bfd88f0d84a23c8d85087
Mohit Patel <mdpatel.csecon@outlook.com>
2025-02-11 22:19:40 -0500

    Removed redundant directory from rename

--- purrnel/src/llk/isa/aarch64/mod.rs
+++
@@ -1,6 +0,0 @@
-pub mod init;
-pub mod interrupts;
-pub mod io;
-pub mod lp_control;
-pub mod memory;
-pub mod system_info;
--- purrnel/src/llk/isa/interface/mod.rs
+++
@@ -1,5 +0,0 @@
-pub mod init;
-pub mod io;
-pub mod lp_control;
-pub mod memory;
-pub mod system_info;
