62e3104fd01e61081721b451c2bff4927b2bc00c
Mohit Patel <mdpatel.csecon@outlook.com>
2025-02-11 22:14:01 -0500

    Rename to Morphism

---
+++ morphism/src/llk/isa/x86_64/init/mod.rs
@@ -0,0 +1,75 @@
+mod gdt;
+
+use core::borrow::{Borrow, BorrowMut};
+use core::ops::Deref;
+// core
+use core::ptr;
+use core::ptr::addr_of;
+
+use exceptions::load_exceptions;
+// crate local
+use gdt::{Gdt, Tss};
+use idt::Idt;
+// external crates
+use lazy_static::lazy_static;
+use spin::Mutex;
+
+use crate::llk::isa::interface::init::InitInterface;
+use crate::llk::isa::x86_64::interrupts::*;
+use crate::{log, logln};
+
+/// The BSP stack size is 4 pages by default.
+const BSP_STACK_SIZE: usize = 4096 * 4;
+
+/// The BSP stack for the kernel.
+/// DO NOT TOUCH THIS, IT IS USED BY THE CPU AS THE KERNEL STACK
+/// UNTIL THE DYNAMIC ALLOCATOR IS INITIALIZED
+#[used]
+static BSP_STACK: [u8; BSP_STACK_SIZE] = [0u8; BSP_STACK_SIZE];
+lazy_static! {
+        /// The Task State Segment for the BSP.
+        /// In long mode, the TSS is used to store the stack pointer for the kernel
+        /// for each privilege level and for interrupts. It also contains the I/O
+        /// permission bitmap which is used to expose or block I/O ports to user-space
+        /// applications.
+
+        static ref BSP_TSS: Mutex<Tss> = Mutex::new(Tss::new(ptr::addr_of!(BSP_STACK) as u64));
+        /// The Global Descriptor Table for the BSP.
+        /// The GDT is used to store the segment descriptors for the kernel and
+        /// user-space applications. In long mode, the GDT is used to store the segment
+        /// descriptors for the kernel and user-space applications. It is largely just a
+        /// leftover in long mode, as segmentation is no longer supported.
+        /// It also contains a system segment descriptor pointing to the TSS which is
+        /// mandatory for interrupts to work.
+        static ref BSP_GDT: Mutex<Gdt> = Mutex::new(Gdt::new(&BSP_TSS.lock()));
+}
+pub struct IsaInitializer;
+
+#[derive(Debug)]
+pub enum Error {
+    InvalidGdt,
+    InvalidTss,
+}
+
+impl InitInterface for IsaInitializer {
+    type Error = Error;
+
+    fn init() -> Result<(), Self::Error> {
+        BSP_GDT.lock().load();
+        logln!("Loaded GDT");
+        Gdt::reload_segment_regs();
+        logln!("Segment registers reloaded");
+        Gdt::load_tss();
+        logln!("Loaded TSS");
+        load_exceptions(IDT.lock().borrow_mut());
+        logln!("Registered exceptions ISRs");
+        IDT.lock().load();
+        logln!("Loaded IDT");
+        Ok(())
+    }
+
+    fn deinit() -> Result<(), Self::Error> {
+        // Nothing to do here yet
+        Ok(())
+    }
+}
---
+++ morphism/src/llk/isa/x86_64/memory/paging/mod.rs
@@ -0,0 +1,102 @@
+pub mod pte;
+pub mod pth_walker;
+
+use core::arch::asm;
+
+use super::MemoryInterfaceImpl;
+use crate::llk::isa::interface::memory::{AddressSpaceInterface, MemoryInterface, MemoryMapping};
+use crate::memory::pmem::VAddr;
+
+pub const PAGE_SIZE: usize = 4096;
+pub const N_PAGE_TABLE_ENTRIES: usize = 512;
+pub type PageTable = [pte::PageTableEntry; N_PAGE_TABLE_ENTRIES];
+
+pub fn is_pagetable_unused(table_ptr: *const PageTable) -> bool {
+    unsafe {
+        for i in 0..N_PAGE_TABLE_ENTRIES {
+            if (*table_ptr)[i].is_present() {
+                return false;
+            }
+        }
+    }
+    true
+}
+
+pub struct AddressSpace {
+    // control register 3 i.e. page table base register
+    cr3: u64,
+}
+
+impl AddressSpaceInterface for AddressSpace {
+    fn get_current() -> Self {
+        let cr3: u64;
+        unsafe {
+            asm!("mov {}, cr3", out(reg) cr3);
+        }
+        AddressSpace { cr3 }
+    }
+
+    fn load(&self) -> Result<(), <MemoryInterfaceImpl as MemoryInterface>::Error> {
+        unsafe {
+            asm!("mov cr3, {}", in(reg) self.cr3);
+        }
+        Ok(())
+    }
+
+    fn find_free_region(
+        &self,
+        n_pages: usize,
+        range: (VAddr, VAddr),
+    ) -> Result<<MemoryInterfaceImpl as MemoryInterface>::VAddr, <MemoryInterfaceImpl as MemoryInterface>::Error> {
+        todo!()
+    }
+
+    fn map_page(&mut self, mapping: MemoryMapping) -> Result<(), <MemoryInterfaceImpl as MemoryInterface>::Error> {
+        let mut walker = pth_walker::PthWalker::new(self, mapping.vaddr);
+        walker.walk()?;
+        walker.map_page(
+            mapping.paddr,
+            mapping.page_type.is_writable(),
+            mapping.page_type.is_user_accessible(),
+            mapping.page_type.is_no_execute(),
+        )?;
+        Ok(())
+    }
+
+    fn unmap_page(
+        &mut self,
+        vaddr: <MemoryInterfaceImpl as MemoryInterface>::VAddr,
+    ) -> Result<(), <MemoryInterfaceImpl as MemoryInterface>::Error> {
+        if <VAddr as Into<usize>>::into(vaddr) == 0 {
+            return Err(<MemoryInterfaceImpl as MemoryInterface>::Error::NullVAddrNotAllowed);
+        }
+        if vaddr.page_offset() != 0 {
+            return Err(<MemoryInterfaceImpl as MemoryInterface>::Error::VAddrNotPageAligned);
+        }
+        let mut walker = pth_walker::PthWalker::new(self, vaddr);
+        walker.unmap_page()?;
+        Ok(())
+    }
+
+    fn is_mapped(
+        &self,
+        vaddr: <MemoryInterfaceImpl as MemoryInterface>::VAddr,
+    ) -> Result<bool, <MemoryInterfaceImpl as MemoryInterface>::Error> {
+        let mut walker = pth_walker::PthWalker::new(self, vaddr);
+        match walker.walk() {
+            Ok(_) => Ok(true),
+            Err(<MemoryInterfaceImpl as MemoryInterface>::Error::Unmapped) => Ok(false),
+            Err(e) => Err(e),
+        }
+    }
+
+    fn translate_address(
+        &self,
+        vaddr: super::address::vaddr::VAddr,
+    ) -> Result<super::address::paddr::PAddr, <MemoryInterfaceImpl as MemoryInterface>::Error> {
+        let mut walker = pth_walker::PthWalker::new(self, vaddr);
+        walker.walk()?;
+        let paddr = unsafe { (*(walker.pt_ptr))[vaddr.pt_index()].get_frame().into() };
+        Ok(paddr)
+    }
+}
