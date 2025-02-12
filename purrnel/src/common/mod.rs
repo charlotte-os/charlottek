fb218eb5be0bac1dbd22abdef037485eb3d3023c
Mohit Patel <mdpatel.csecon@outlook.com>
2025-02-11 22:19:40 -0500

    Removed redundant directory from rename

--- purrnel/src/common/mod.rs
+++
@@ -1,8 +0,0 @@
-//! # Common Utilities for use throughout Purrnel
-//!
-//! This module can essentially be thought of as an extension to the Rust core library. It provides standard library
-//! like functionality for the Purrnel kernel. This  module should not duplicate functionality that is already provided
-//! by core or any other high quality no_std library unless there is a compelling reason to do so.
-
-pub mod cell;
-pub mod io;
--- purrnel/src/llk/isa/x86_64/interrupts/mod.rs
+++
@@ -1,9 +0,0 @@
-pub mod exceptions;
-pub mod idt;
-
-use core::mem::MaybeUninit;
-
-use idt::*;
-use spin::Mutex;
-
-pub static IDT: Mutex<Idt> = Mutex::new(Idt::new());
