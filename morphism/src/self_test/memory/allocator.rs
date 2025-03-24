7ef996e84cb5e3e06ca53e88d132c0b9a478ecc8
Mohit D Patel <mdpatel.csecon@outlook.com>
2025-03-23 21:48:57 -0400

    Started implementing the kernel allocator.

--- morphism/src/self_test/memory/allocator.rs
+++ morphism/src/self_test/memory/allocator.rs
@@ -1,1 +1,21 @@
+use core::mem::MaybeUninit;
+use core::ptr::NonNull;
 
+use crate::llk::isa::x86_64::memory::paging::PAGE_SIZE;
+
+struct PageLevelAllocator {
+    heap_start:  VAddr,
+    heap_npages: usize,
+    heap_bitmap: *mut u8,
+}
+
+const SLAB_ALLOCATION_REGION_SIZE: usize = PAGE_SIZE * 16;
+
+struct SlabAllocationRegion<const N_BYTES: usize> {
+    next: Option<NonNull<SlabAllocationRegion<N_BYTES>>>,
+    data: [u8; N_BYTES],d
+}
+
+struct WordLevelAllocator {
+    slab_alloc_regions8: Option<NonNull<u8>>,
+}
