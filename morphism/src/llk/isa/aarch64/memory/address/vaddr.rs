97f4cd85ca6a4f35e812c196f493d64a9f94f4a4
Mohit D Patel <mdpatel.csecon@outlook.com>
2025-03-29 20:30:14 -0400

    Refactor address traits to unify common functionality of the VirtualAddress and PhysicalAddress traits into a common base trait called Address.

--- morphism/src/llk/isa/aarch64/memory/address/vaddr.rs
+++ morphism/src/llk/isa/aarch64/memory/address/vaddr.rs
@@ -1,7 +1,33 @@
-use crate::llk::isa::interface::memory::address::VirtualAddress;
+use crate::llk::isa::interface::{
+    system_info::CpuInfoIfce,
+    memory::address::{Address, VirtualAddress}
+};
+use crate::llk::isa::aarch64::system_info::CpuInfo;
 
 #[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Debug)]
 pub struct VAddr(usize);
+
+impl Address for VAddr {
+    const MIN: Self = VAddr(0);
+    const MAX: Self = VAddr(usize::MAX);
+    const NULL: Self = VAddr(0);
+
+    fn is_aligned_to(&self, alignment: usize) -> bool {
+        self.0 % alignment == 0
+    }
+
+    fn is_valid(value: usize) -> bool {
+        value & ((1 << CpuInfo::get_paddr_sig_bits()) - 1) == value
+    }
+
+    fn is_null(&self) -> bool {
+        self.0 == 0
+    }
+
+    fn next_aligned_to(&self, alignment: usize) -> Self {
+        VAddr((self.0 + alignment - 1) & !(alignment - 1))
+    }
+}
 
 impl VirtualAddress for VAddr {
     fn from_ptr<T>(ptr: *const T) -> Self {
