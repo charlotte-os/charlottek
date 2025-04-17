4a272d0cc8da5414c878ab53035be2452f24866a
Mohit Patel <mdpatel.csecon@outlook.com>
2025-04-17 01:07:33 -0400

    Change references to Purrnel in comments and doc comments to Morphism, the current name of this kernel.

--- morphism/src/common/mod.rs
+++ morphism/src/common/mod.rs
@@ -1,7 +1,7 @@
-//! # Common Utilities for use throughout Purrnel
+//! # Common Utilities for use throughout Morphism
 //!
 //! This module can essentially be thought of as an extension to the Rust core library. It provides
-//! standard library like functionality for the Purrnel kernel. This  module should not duplicate
+//! standard library like functionality for the Morphism kernel. This  module should not duplicate
 //! functionality that is already provided by core or any other high quality no_std library unless
 //! there is a compelling reason to do so.
 
