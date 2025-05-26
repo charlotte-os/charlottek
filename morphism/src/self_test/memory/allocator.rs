1a3de217d306656673b5ad881b665f69f286b008
Mohit D Patel <mdpatel.csecon@outlook.com>
2025-05-26 01:12:59 -0400

    Made correction to the vector implementation and made the log macros more self reliant. Reenabled serial logging on the x86_64 target.

--- morphism/src/self_test/memory/allocator.rs
+++ morphism/src/self_test/memory/allocator.rs
@@ -7,25 +7,25 @@ pub fn test_allocator() {
     logln!("Starting the kernel allocator self-test...");
     logln!("Kernel allocator self-test: Allocating 1024 bytes...");
     let ptr = KERNEL_ALLOCATOR
-        .allocate(Layout::from_size_align(1024, 8).unwrap())
+        .allocate(Layout::from_size_align(1050, 64).unwrap())
         .unwrap()
         .as_non_null_ptr();
     logln!("Kernel allocator self-test: Allocated 1024 bytes at {:p}", ptr);
     logln!("Kernel allocator self-test: Writing to allocated memory...");
-    for i in 0..1024 {
+    for i in 0..1050 {
         unsafe {
             ptr.add(i).write(i as u8);
         }
     }
     logln!("Kernel allocator self-test: Write complete.");
     logln!("Kernel allocator self-test: Reading from allocated memory...");
-    for i in 0..1024 {
+    for i in 0..1050 {
         assert_eq!(unsafe { ptr.offset(i).read() }, i as u8);
     }
     logln!("Kernel allocator self-test: Read complete.");
     logln!("Kernel allocator self-test: Deallocating allocated memory...");
     unsafe {
-        KERNEL_ALLOCATOR.deallocate(ptr, Layout::from_size_align(1024, 8).unwrap());
+        KERNEL_ALLOCATOR.deallocate(ptr, Layout::from_size_align(1050, 64).unwrap());
     }
     logln!("Kernel allocator self-test: Deallocation complete.");
     logln!("Kernel allocator self-test: Allocating 8 KiB...");
