ea9169d62fac1a716f3767e518908a1933719476
Mohit D Patel <mdpatel.csecon@outlook.com>
2025-07-06 04:19:31 -0400

    Removed unused vector module. Set up the talck as the global allocator to allow the use of alloc. Started implementing threading.

--- charlottek/src/tsmp/mp.rs
+++ charlottek/src/tsmp/mp.rs
@@ -1,8 +1,24 @@
 //! # Multi-Processor Control
+use core::mem::MaybeUninit;
+
 use limine::mp::Cpu;
+
+use spin::{Lazy, RwLock};
 
 use crate::environment::boot_protocol::limine::MP;
 use crate::{ap_main, logln};
+
+static LP_COUNT: RwLock<Lazy<usize>> = RwLock::new(Lazy::new(|| {
+    if let Some(mp_res) = MP.get_response() {
+        mp_res.cpus().len()
+    } else {
+        panic!("Limine was not able to start the secondary logical processors!")
+    }
+}));
+
+pub fn get_lp_count() -> usize {
+    **LP_COUNT.read()
+}
 
 #[derive(Debug)]
 pub enum MpControlError {
