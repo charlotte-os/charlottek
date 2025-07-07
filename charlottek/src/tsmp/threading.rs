02ccab78a7d5f67eb3da2deee70570ccb9cb2d4d
Mohit D Patel <mdpatel.csecon@outlook.com>
2025-07-06 23:52:26 -0400

    Refactor LpControl interface and update threading implementation to use BTreeMap for thread management
    Signed-off-by: Mohit D Patel <mdpatel.csecon@outlook.com>

--- charlottek/src/tsmp/threading.rs
+++ charlottek/src/tsmp/threading.rs
@@ -1,4 +1,5 @@
 use alloc::boxed::Box;
+use alloc::collections::btree_map::BTreeMap;
 use alloc::collections::VecDeque;
 
 use spin::Mutex;
@@ -10,8 +11,25 @@ use super::mp::get_lp_count;
 
 pub type ThreadId = u64;
 pub type LpState = <LpControl as LpControlIfce>::LpState;
+pub type LpId = <LpControl as LpControlIfce>::LpId;
 
-static THREAD_TABLE: VecDeque<Mutex<Thread>> = VecDeque::new();
+static THREAD_TABLE: BTreeMap<LpId, Mutex<Option<Thread>>> = BTreeMap::new();
+
+pub enum ThreadingError {
+    LpDoesNotExist,
+}
+
+pub fn get_current_tid() -> Result<Option<ThreadId>, ThreadingError> {
+    if let Some(thread_lock) = THREAD_TABLE.get(&LpControl::get_lp_id()) {
+        let thread = thread_lock.lock();
+        match thread.as_ref() {
+            Some(t) => Ok(Some(t.id)),
+            None => Ok(None),
+        }
+    } else {
+        Err(ThreadingError::LpDoesNotExist)
+    }
+}
 
 #[repr(C, packed)]
 pub struct Thread {
