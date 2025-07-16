c635daa7927ee332181627a6c0f35e64e44d2cf0
Mohit D Patel <mdpatel.csecon@outlook.com>
2025-07-16 17:22:33 -0400

    Refactor interrupt and LP control interfaces; implement state saving/loading in assembly; update threading management with BTreeMap.
    Signed-off-by: Mohit D Patel <mdpatel.csecon@outlook.com>

--- charlottek/src/isa/interface/lp_control/mod.rs
+++ charlottek/src/isa/interface/lp_control/mod.rs
@@ -1,3 +1,5 @@
+use alloc::boxed::Box;
+
 pub trait LpControlIfce {
     type LpId;
     type LpState;
@@ -7,6 +9,9 @@ pub trait LpControlIfce {
     fn mask_interrupts();
     fn unmask_interrupts();
     fn get_lp_id() -> Self::LpId;
-    extern "C" fn save_lp_state() -> Result<(), Self::Error>;
-    extern "C" fn load_lp_state() -> Result<(), Self::Error>;
+    /// Saves the current logical processor state and returns it as a boxed value.
+    /// This function must be declared as inline(always) to ensure no register state is lost.
+    fn save_lp_state() -> Box<Self::LpState>;
+    /// Loads the logical processor state from a previously saved state.
+    fn load_lp_state(state: Box<Self::LpState>);
 }
--- charlottek/src/tsmp/threading.rs
+++ charlottek/src/tsmp/threading.rs
@@ -1,26 +1,27 @@
 use alloc::boxed::Box;
-use alloc::collections::btree_map::BTreeMap;
 use alloc::collections::VecDeque;
+use alloc::collections::btree_map::BTreeMap;
+use alloc::vec::Vec;
 
 use spin::Mutex;
 
-use crate::isa::interface::lp_control::LpControlIfce;
-use crate::isa::current_isa::lp_control::LpControl;
-
 use super::mp::get_lp_count;
+use crate::isa::current_isa::lp_control::LpControl;
+use crate::isa::interface::lp_control::LpControlIfce;
 
 pub type ThreadId = u64;
 pub type LpState = <LpControl as LpControlIfce>::LpState;
 pub type LpId = <LpControl as LpControlIfce>::LpId;
 
-static THREAD_TABLE: BTreeMap<LpId, Mutex<Option<Thread>>> = BTreeMap::new();
+static THREADS_IN_FLIGHT: BTreeMap<LpId, Mutex<Option<Thread>>> = BTreeMap::new();
+static mut THREAD_TABLE: BTreeMap<ThreadId, Mutex<Thread>> = BTreeMap::new();
 
 pub enum ThreadingError {
     LpDoesNotExist,
 }
 
 pub fn get_current_tid() -> Result<Option<ThreadId>, ThreadingError> {
-    if let Some(thread_lock) = THREAD_TABLE.get(&LpControl::get_lp_id()) {
+    if let Some(thread_lock) = THREADS_IN_FLIGHT.get(&LpControl::get_lp_id()) {
         let thread = thread_lock.lock();
         match thread.as_ref() {
             Some(t) => Ok(Some(t.id)),
