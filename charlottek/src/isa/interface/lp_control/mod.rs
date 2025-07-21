b7c29c5506ca7374f6b3713a8fc5033f17f5be5f
Mohit D Patel <mdpatel.csecon@outlook.com>
2025-07-21 02:26:05 -0400

    Update dependencies and refactor threading and multiprocessing modules; remove tsmp module; continue development of context switching code
    Signed-off-by: Mohit D Patel <mdpatel.csecon@outlook.com>

--- charlottek/src/isa/interface/lp_control/mod.rs
+++ charlottek/src/isa/interface/lp_control/mod.rs
@@ -1,15 +1,12 @@
-use alloc::boxed::Box;
-
-use crate::tsmp::threading::ThreadId;
-
+//! # x86_64 Logical Processor Control Interface
 pub trait LpControlIfce {
     type LpId;
-    type LpState;
     type Error;
 
     fn halt() -> !;
     fn mask_interrupts();
     fn unmask_interrupts();
     fn get_lp_id() -> Self::LpId;
-    extern "C" fn switch_context() -> !;
+    extern "C" fn switch_context();
+    extern "C" fn load_context();
 }
--- charlottek/src/tsmp/mod.rs
+++
@@ -1,5 +0,0 @@
-//! # Threading, Synchronization, and multi-processor operations
-
-pub mod mp;
-pub mod sync;
-pub mod threading;
